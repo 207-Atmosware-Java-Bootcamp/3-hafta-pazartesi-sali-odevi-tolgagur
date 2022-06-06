c4f3153ebec9bfe48449169bd9617ae1c19810f7
Tolga Gureli <tolgagureli@outlook.com>
2022-06-06 17:28:54 +0300

    edit

--- odev1/template.js
+++ odev1/template.js
@@ -1,3 +1,17 @@
+//ÖDEV: Math.round(Math.random()*10+1))
+// bilgisayar tarafında rastgele  sayılar 1-10 arasında rastgele olsun oluştursun ve bitiş değeri 1<=X<=kullanıcı tarafından bitiş sayısına göre sayılar oluşturulsun
+// bu sayıları bir diziye atama yapalım
+// bu sayılardan ilk eleman  ===> dizi[0]
+// bu sayılardan son eleman ===> dizi[dizi.length-1]
+// bu sayıların küçükten büyüğe sıralama ===> dizi.sort()
+// bu sayıların büyükten küçüğe sıralama ===> dizi.sort().reverse()
+// bu sayıların toplamları  ===> sum=0  sum+=i;
+// bu sayıların çift sayı toplamları  ===> if(dizi[i]%2==0)
+// bu sayıların tek sayı toplamları  ===> if(dizi[i]%2==1)
+// bu sayıların her birine 1 ekleyerek yeni bir dizi oluşturalım ==> iterative for ile her bir eleman erişmek ve 1 eklemek bunun dinamik kısmını ES kısmında göreceğiz.
+
+//Not çıktılar console'da gösterilmektedir.
+
 
 // kullanıcıdan sayı girmesinin beklendiği yer
 var number= prompt("Dizi boyutunu giriniz.");
