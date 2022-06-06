818efd393299ba18b879f9fc99fdc5f198209479
Tolga Gureli <tolgagureli@outlook.com>
2022-06-06 19:57:44 +0300

    3 homework

---
+++ odev3/odev.js
@@ -0,0 +1,27 @@
+//Kullanıcıdan aldığımız verinin ilk harfi ve son harfi görünsün geriye kalan karakter
+// kadar * (yıldız) olsun ==> Alınan örneğin Hamit  ==>  H***t
+//Yardımcı oalcak metotlar v.s function
+//string ==> replace,sub,String,charAt()
+//loop
+
+
+var str = prompt("Adınızı giriniz.");
+
+//while ile kullanıcının geçerli bir veri girmesini sağlıyoruz
+while(!(str.length>2)){
+    alert("2 den çok karakter olmalı");
+    var str= prompt("adınızı giriniz.");
+}
+
+function valid() { 
+    var strSec = "";
+
+    for (let i = 0; i < str.length-2; i++) {
+        strSec = strSec.concat("*")  ;     
+    }
+
+    return (str[0].toLocaleUpperCase() + strSec + str[str.length-1]); 
+ }
+
+ document.write("Gizlenen veri : " + valid());
+ console.log("test : "  + str);
